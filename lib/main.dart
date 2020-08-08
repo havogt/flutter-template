2eb2ee3e047c2d245f8789854efeb04e42564f9a
Hannes Vogt <hannes@havogt.de>
2020-08-08 21:35:23 +0000

    add push_notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_template/push_notifications.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,6 +9,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    var pushNotification = PushNotificationsManager();
+    pushNotification.init();
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
